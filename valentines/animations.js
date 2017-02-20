297f890cc84949c2713a3f1d0a656e27bc6e1368
jrodonnell909 <jrodonnell909@gmail.com>
2017-02-20 11:43:44 -0500

    Removed unnecessary directory

--- valentines/animations.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#scale-demo').click(function() {
-        $('#page-title').show();
-        $('#card').show();
-        $('#click-me').hide();
-        $('#extra-space').hide();
-    });
-});
